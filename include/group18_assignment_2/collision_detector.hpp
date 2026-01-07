5825a7b45ab435c57d6c1aa7b902bcd2520bfc00
marcofacco2001 <marco.facco.4@studenti.unipd.it>
2026-01-07 13:03:03 +0100

    Add collision_detector executable and update related files
    - Introduced collision_detector executable in CMakeLists.txt
    - Updated collision_detector.hpp and collision_detector.cpp for consistency and improved logging messages

--- include/group18_assignment_2/collision_detector.hpp
+++ include/group18_assignment_2/collision_detector.hpp
@@ -1,21 +1,21 @@
 #ifndef COLLISION_DETECTOR_HPP_
 #define COLLISION_DETECTOR_HPP_
 
-// 1. Core ROS 2
+// Core ROS 2
 #include <rclcpp/rclcpp.hpp>
 #include <rclcpp/node_options.hpp>
 
-// 2. Standard C++
+// Standard C++
 #include <memory>
 #include <string>
 #include <chrono>
 
-// 3. TF2 Headers
+// TF2 Headers
 #include <tf2_ros/transform_listener.h>
 #include <tf2_ros/buffer.h>
 #include <tf2_geometry_msgs/tf2_geometry_msgs.hpp>
 
-// 4. MoveIt Headers
+// MoveIt Headers
 #include <moveit/move_group_interface/move_group_interface.hpp>
 #include <moveit/planning_scene_interface/planning_scene_interface.hpp>
 
